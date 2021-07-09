1878e11f6ed3c780ff3db86b0b06b650adc63757
suhasns007 <58617968+suhasns007@users.noreply.github.com>
2021-07-10 01:52:29 +0530

    Add files via upload

---
+++ citizensignin.php
@@ -0,0 +1,27 @@
+<?php
+
+$con = new mysqli("localhost", "root", "", "govt_db");
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+// username and password sent from form 
+$userid=$_POST['citiusername']; 
+$password=$_POST['citipassword']; 
+
+
+
+$rec=mysqli_query($con, "SELECT * FROM citizen_tab WHERE citiusername='$userid' AND citipassword='$password'");
+
+$noticia = mysqli_fetch_assoc($rec);
+
+if($noticia)
+{
+$_SESSION['login_user']=$userid;
+header("location:citipage.php?myaccount=$userid");
+}
+else
+{
+echo "<font face='Verdana' size='2' color=red><center> Use citizen details to Login and try<br> <br><input type='button' value='Retry' onClick='history.go(-1)'></center>";
+}
+}
+?>
