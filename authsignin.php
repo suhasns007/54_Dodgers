f9c8740c3fcf5a067538b3fa109b5b05ace94ac9
suhasns007 <58617968+suhasns007@users.noreply.github.com>
2021-07-10 01:40:25 +0530

    Add files via upload

---
+++ authsignin.php
@@ -0,0 +1,27 @@
+<?php
+
+$con = new mysqli("localhost", "root", "", "govt_db");
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+// username and password sent from form 
+$userid=$_POST['authusername']; 
+$password=$_POST['authpassword']; 
+
+
+
+$rec=mysqli_query($con, "SELECT * FROM auth_tab WHERE ID='$userid' AND PASSWORD='$password'");
+
+$noticia = mysqli_fetch_assoc($rec);
+
+if($noticia)
+{
+$_SESSION['login_user']=$userid;
+header("location:authpage.php?myaccount=$userid");
+}
+else
+{
+echo "<font face='Verdana' size='2' color=red><center> Use authority details to Login and try<br> <br><input type='button' value='Retry' onClick='history.go(-1)'></center>";
+}
+}
+?>
