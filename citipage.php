17a79203f2451a8fd6c6028c32e19178834a5db6
suhasns007 <58617968+suhasns007@users.noreply.github.com>
2021-07-10 23:46:32 +0530

    Update citipage.php

--- citipage.php
+++ citipage.php
@@ -1,14 +1,87 @@
 <?php
 <html>
-    <head>
-        <title>Queries page</title>
-    </head>
-    <body>
-        <textarea name="complaint" rows="20" class="form-control" placeholder="Complaint box" message="message"></textarea>
+<head>
+  <link rel="stylesheet" href="./css/electricityissues.css">
+</head>
+<body>
+    <center>
+    <div class="departments" style="background-color:cyan">
+        <h1>List of Departments</h1>
+        <h3><marquee>Click on the department to raise the related issues</marquee></h3>
+</div></center>
+<br><br>
 
-        <br><br>
+	<article
+	 style="width:300px;
+            height:400px;
+			border:2px solid gray;
+			padding:10px;
+			border-radius:10px;
+			margin-left:40px;
+            margin-top:10px
+            float:center">
+		
+	<img src="" alt="not available" width="300" height="250"
+	 class="alignnone size-medium wp-image-560930"/>
+	
+	
+	
+	<center><h1> Electricity Department</h1>
+	<a href="electricityissues.php"><button class="issuesubmit">Click here</button></a>
+	</center>
+	</article>
+    
 
-        <input type="submit" value='Submit my complaint'>
+ 
+    <article
+	 style="width:300px;
+            height:400px;
+			border:2px solid gray;
+			padding:10px;
+			border-radius:10px;
+			margin-left:600px;
+            margin-top:-425px;
+            float:center;
+	">
+		
+	<img src="" alt="not available" width="300" height="250"
+	 class="alignnone size-medium wp-image-560930"/>
+	
+	
+	
+     <center><h1>Municipality Department</h1>
+	<a href="municipalityissues.php"><button class="issuesubmit">Click here</button></a>
+	</center>
+	
+	</article>
+    
+
+    <article
+	 style="width:300px;
+            height:400px;
+			border:2px solid gray;
+			padding:10px;
+			border-radius:10px;
+			margin-left:1150px;
+            margin-top:-430px;
+            float:center;
+	">
+		
+	<img src="" alt="not available" width="300" height="250"
+	 class="alignnone size-medium wp-image-560930"/>
+	
+	
+	
+     <center><h1>Income Tax Department</h1>
+	<a href="itissues.php"><button class="issuesubmit">Click here</button></a>
+	</center>
+	
+	</article>
+
+
+
+
 </body>
+
 </html>
-?>
+    ?>
