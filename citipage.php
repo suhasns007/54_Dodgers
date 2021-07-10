1b696e6439a8c9a53a08854501c25e915c9add8c
Naresh36 <61237204+Naresh36@users.noreply.github.com>
2021-07-10 09:39:48 +0530

    Add files via upload

---
+++ citipage.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Queries page</title>
+    </head>
+    <body>
+        <textarea name="complaint" rows="20" class="form-control" placeholder="Complaint box" message="message"></textarea>
+
+        <br><br>
+
+        <input type="submit" value='Submit my complaint'>
+</body>
+</html>
