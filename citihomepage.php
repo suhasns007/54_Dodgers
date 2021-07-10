c5bcd823169b2dafd65fe4a98be4f18b25bd81f6
Vinay KB <62440408+vinay-kb@users.noreply.github.com>
2021-07-10 14:46:26 +0530

    Add files via upload

---
+++ citihomepage.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Citizen home page</title>
+    </head>
+    <body>
+        <div class="displayqueries">
+            <a href="displayqueries.php" target="_self"> Display queries raised by other citizens</a>
+        </div>
+        <div class="citipage">
+            <br><br>
+            <a href="citipage.php" target="_self"> Go to citizen page</a>
+        </div>
+    </body>
+</html>
