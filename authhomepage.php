d5f1c8c0990667f64c4eda17ca513d03904d65ed
SuchitraH17 <87165315+SuchitraH17@users.noreply.github.com>
2021-07-10 14:59:27 +0530

    Add files via upload

---
+++ authhomepage.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Authority homepage</title>
+</head>
+<body>
+    <div class="displayissues">
+        <a href="displayissues.php" target="_self">Display cissues rasied by citizens</a>
+    </div>
+    <div class="displaycitizens">
+        <a href="displaycitizens.php" target="_self">Display citizens and their details</a>
+    </div>
+</body>
+</html>
