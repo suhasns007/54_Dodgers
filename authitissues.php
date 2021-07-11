c2acd26beb3f35651eca3a5a4c2f6928d4bfec6b
suhasns007 <58617968+suhasns007@users.noreply.github.com>
2021-07-11 10:06:17 +0530

    Update authitissues.php

--- authitissues.php
+++ authitissues.php
@@ -11,7 +11,24 @@
         <h3><marquee>View related issues in the below section</marquee></h3>
 </div>
 <div class="authitissues">
-
+<?php
+    $con = new mysqli("localhost", "root", "", "govt_db");
+        $sql1="SELECT * FROM `it_issues`";
+        $result=mysqli_query($con, $sql1);
+        while($row=mysqli_fetch_assoc($result)){
+            // echo("<br><br>");
+            // echo $row['issue_id'];
+            // echo("&nbsp;&nbsp;");
+            // echo $row['username'];
+            // echo("<br>");
+            // echo $row['itcomplaint'];
+            echo "Issue_ID :{$row['issue_id']}  <br> ".
+                "Citizen Name : {$row['username']} <br> ".
+               "Issue : {$row['itcomplaint']} <br> ".
+                "--------------------------------<br>";
+          }
+          echo "<br><br><a href='index.html' target='_self'>Click here to return to home</a>";
+        ?>
 </div>
 
 </body>
